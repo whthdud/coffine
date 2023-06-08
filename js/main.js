7b3e0463e7d9c4a184abd6a7d2b3a330a0b173a8
whthdud <jwh442@naver.com>
2023-06-08 14:21:23 +0900

    커핀그루나루첫날

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(function(){
+
+
+    //메뉴
+
+    $(".sub").hide();
+
+    $(".gnb").mouseenter(function(){
+        $(".sub").stop().slideDown();
+        $(".header_b").stop().animate({height:"400px"},500)
+    })//mouseenter
+
+    $(".gnb").mouseleave(function(){
+        $(".sub").stop().slideUp();
+        $(".header_b").stop().animate({height:"103px"},500)
+    })//mouseleave
+
+
+    //이미지 슬라이드 영역
+
+    $(".move").slick({
+        autoplay:true,
+        autoplaySpeed:2000,
+        dots:true,
+        arrows:true
+    })
+
+})//close
